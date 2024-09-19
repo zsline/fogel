199d71f293b21b1b1227e53fa150b5d6113c96b8
zsline <sergei.dereka@gmail.com>
2024-09-19 20:15:51 +0300

    offer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
-
+document.querySelector('.menu-btn').addEventListener('click', function(){
+    this.classList.toggle('open');
+    document.querySelector('.nav').classList.toggle('open');
+    document.querySelector('body').classList.toggle('noscroll');
+})
