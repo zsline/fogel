43b0eb73ca8f89693b20ee267c96382e85a4d2b9
zsline <sergei.dereka@gmail.com>
2024-09-21 16:25:19 +0300

    quote-2

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,23 @@ document.querySelector('.menu-btn').addEventListener('click', function(){
     this.classList.toggle('open');
     document.querySelector('.nav').classList.toggle('open');
     document.querySelector('body').classList.toggle('noscroll');
-})
+});
+new Swiper('.result__slider', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.result-next',
+      prevEl: '.result-prev',
+    },
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        647: {
+          slidesPerView: 2,
+        },
+        960: {
+          slidesPerView: 3,
+        }
+      }
+});
