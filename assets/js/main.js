a130e2c3f9f59fd6805e94f63cbd9bdd345914eb
zsline <sergei.dereka@gmail.com>
2024-09-23 15:28:27 +0300

    methods -> experience

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,31 @@
-document.querySelector('.menu-btn').addEventListener('click', function(){
-    this.classList.toggle('open');
-    document.querySelector('.nav').classList.toggle('open');
-    document.querySelector('body').classList.toggle('noscroll');
+document.querySelector('.menu-btn').addEventListener('click', function () {
+  this.classList.toggle('open');
+  document.querySelector('.nav').classList.toggle('open');
+  document.querySelector('body').classList.toggle('noscroll');
 });
+
+// const experienceImage = document.querySelector('.experience__images');
+// const experience = document.querySelector('.experience');
+// experience.style.minHeight = experienceImage.scrollHeight + 'px';
+// experience.querySelector('.experience__wrapper').style.paddingLeft = experienceImage.scrollWidth + 'px';
 
 
 // =============== FAQ ==================
 
 const faqItems = document.querySelectorAll('.faq__item');
-faqItems.forEach(el =>{
-  el.firstElementChild.addEventListener('click', function() {
-    faqItems.forEach(item =>{
+faqItems.forEach(el => {
+  el.firstElementChild.addEventListener('click', function () {
+    faqItems.forEach(item => {
       item.classList.remove('open');
     });
-  el.classList.add('open')
+    el.classList.add('open')
   });
 });
 
 
 // ================= SLIDERS ==============
-new Swiper('.result__slider', {
+if (document.querySelector('.result__slider')) {
+  new Swiper('.result__slider', {
     slidesPerView: 3,
     spaceBetween: 30,
     navigation: {
@@ -27,20 +33,24 @@ new Swiper('.result__slider', {
       prevEl: '.result-prev',
     },
     breakpoints: {
-        320: {
-          slidesPerView: 1,
-        },
-        647: {
-          slidesPerView: 2,
-        },
-        960: {
-          slidesPerView: 3,
-        }
+      320: {
+        slidesPerView: 1,
+      },
+      647: {
+        slidesPerView: 2,
+      },
+      960: {
+        slidesPerView: 3,
       }
-});
-new Swiper('.reviews__slider',{
-  navigation: {
-    nextEl: '.reviews-next',
-    prevEl: '.reviews-prev',
-  },
-});
+    }
+  });
+}
+if (document.querySelector('.reviews__slider')) {
+  new Swiper('.reviews__slider', {
+    navigation: {
+      nextEl: '.reviews-next',
+      prevEl: '.reviews-prev',
+    },
+  });
+}
+
